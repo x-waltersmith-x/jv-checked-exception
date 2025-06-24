e0d361b46b8a06e52313ead648f84885c945d54c
Volodymyr Kovalov <vkovalov@Volodymyrs-Mac-mini.local>
2025-06-24 22:03:16 +0200

    I forgot to check style mistakes

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,8 +5,7 @@ public class PasswordValidator {
             throws PasswordValidationException {
         if (password.length() < 10) {
             throw new PasswordValidationException("Password must be 10 characters minimum");
-        }
-        else if (!password.equals(repeatPassword)) {
+        } else if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
