3cf2f22d1902b1ac9f8af1f75a23848423a30b0b
Volodymyr Kovalov <vkovalov@Volodymyrs-Mac-mini.local>
2025-06-24 22:02:00 +0200

    Fix nullPointerException

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,10 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (!password.equals(repeatPassword) || password.length() < 10) {
+        if (password.length() < 10) {
+            throw new PasswordValidationException("Password must be 10 characters minimum");
+        }
+        else if (!password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
