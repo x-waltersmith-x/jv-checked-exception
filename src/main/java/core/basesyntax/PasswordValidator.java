72c4a5ff32c057a002638b18a0b40c66d257de2c
Volodymyr Kovalov <vkovalov@Volodymyrs-Mac-mini.local>
2025-06-24 22:05:03 +0200

    Fix wrong message

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,9 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password.length() < 10) {
-            throw new PasswordValidationException("Password must be 10 characters minimum");
-        } else if (!password.equals(repeatPassword)) {
+        if (password.length() < 10  && !password.equals(repeatPassword)) {
             throw new PasswordValidationException("Wrong passwords");
         }
     }
